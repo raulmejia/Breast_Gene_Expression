3c025b521abbefb58ca29d66a9c3490d66dbb631
raulmejia <raulmejia@users.noreply.github.com>
2015-12-06 20:17:26 -0600

    Update Making_eSet.R

--- Individual_Functions/Making_eSet.R
+++ Individual_Functions/Making_eSet.R
@@ -1,14 +1,11 @@
 
 
-exprsFile="discovery_ExpressionMatrix.txt"
-
-
-
-
+exprsFile=".txt"
 library("Biobase")
 exprs <- as.matrix(read.table(exprsFile, header=TRUE, sep="\t", row.names=1, as.is=TRUE))
 minimalSet <- ExpressionSet(assayData=exprs)
 
+###############
 
 exprsFileNorm="../Normals/normals_ExpressionMatrix.txt"
 library("Biobase")
@@ -16,7 +13,20 @@ Normexprs <- as.matrix(read.table(exprsFileNorm, header=TRUE, sep="\t", row.name
 NormalminimalSet <- ExpressionSet(assayData=Normexprs)
 
 proc.time()
-exprsFileNorm="../Normals/normals_ExpressionMatrix.txt"
+
+exprsFileDisc="../DiscoverySet_Complete_4800/discovery_ExpressionMatrix.txt"
 library("Biobase")
-Normexprs <- as.matrix(read.table(exprsFileNorm, header=TRUE, sep="\t", row.names=1, as.is=TRUE))
-NormalminimalSet <- ExpressionSet(assayData=Normexprs)
+Discexprs<- as.matrix(read.table(exprsFileDisc, header=TRUE, sep="\t", row.names=1, as.is=TRUE))
+DiscminimalSet <- ExpressionSet(assayData=Discexprs)
+
+
+exprsFileVal="../ValidationSet/validation_ExpressionMatrix.txt"
+Valexprs<- as.matrix(read.table(exprsFileVal, header=TRUE, sep="\t", row.names=1, as.is=TRUE))
+ValminimalSet <- ExpressionSet(assayData=Valexprs)
+
+
+Matrix.METABRIC<-cbind(Normexprs,Discexprs,Valexprs)
+eSet.METABRIC<-ExpressionSet(assayData=Matrix.METABRIC)
+
+###  To Do
+# Read the data with more significant digits

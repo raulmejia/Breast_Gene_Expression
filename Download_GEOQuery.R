043bda326f1bb3f8028bbf93181a947843483ca9
raulmejia <raulmejia@users.noreply.github.com>
2016-01-27 21:07:29 -0600

    Update Download_GEOQuery.R

--- Download_GEOQuery.R
+++ Download_GEOQuery.R
@@ -1,16 +1,32 @@
-###
-### Script made Wed Nov  4 19:52:46 CST 2015
-### R version 3.2.0 (2015-04-16) -- "Full of Ingredients"
+###   Script made Wed Nov  4 19:52:46 CST 2015
+###   R version 3.2.0 (2015-04-16) -- "Full of Ingredients"
 ###   Platform: x86_64-unknown-linux-gnu (64-bit)
 ###   GEOquery_2.34.0     Biobase_2.28.0      BiocGenerics_0.14.0
-###   
+
 #install GEOquery
 source("http://bioconductor.org/biocLite.R")
 biocLite("GEOquery")
 library(GEOquery)
+
+############################################################################  
+# If you have the GSM vector try to Download the .CEL files through:     ###
+############################################################################
+
+# You need your own GSM vector, in this example, we'll use 
+#################Exaple GSM vector##################
+MyGSMList = c("GSM272923","GSM272924","GSM272925")##
+####################################################
+sapply(MyGSMList,function(x){ getGEOSuppFiles(x)})
+
+
+
+
 ###################################################################
 # If you only have a GSE's list use the next chunk of code ########
 ###################################################################
+
+#This couple of functions download the .CEl.gz files asociated with your GSEs 
+# and also retrieves you  a vector with the GSM names.
 
 GSMnamesFromVecGSEnames<- function(vecGSEnames){
   #This function recieves a vector of GSEnames
@@ -39,62 +55,6 @@ NamesVectorGSMFromGSE <-function(ThisGSEObject){
   return(vecpartialnames)
 }  
 
-############################################################################  
-#Once we have a GSMnames vector we can Download the .CEL files through:  ###
-############################################################################
-  
-#Función calada para descargar los datos supplementarios (Incluyendo los CEL files) a partir de un vector de nombres GSM
-#Para comenzar desde donde se quedo y evitar el problema es que las descargas se cortan  
-DownCELFromThisGSMVec<-function(ThisGSMList){
-    for(i in 1:5){
-    sapply(ThisGSMList,function(x){ getGEOSuppFiles(x)})
-    #ponle un if para comprobar exito en la descarga si no que comience
-      if( length(ThisGSMList) == as.numeric(try(system("ls GSM*/*CEL* |  wc -l"))) ) {
-      return()
-      } 
-    }
-  }
-
-#####################################################
-###### More pseudo-random code in construction ######
-#####################################################
-
-
-  RobustAllGSMfromthisGSMList<-function(ThisGSMList){
-    for(i in 1:20){
-    sapply(ThisGSMList,function(x){ getGEOSuppFiles(x)})
-    #ponle un if para comprobar exito en la descarga si no que comience
-    
-      if( length(ThisGSMList) == as.numeric(try(system("ls GSM*/*CEL* |  wc -l"))) ) {
-      return()
-      } 
-      # Obten un archivo csv a partir de los nombres de los .CEL ya descargados
-      (try(system("ls GSM*/*CEL* |  sed -e 's/\//_/g' already | sed 's/\./_/g' | sed 's/_/"\t"/g' | awk '{ print $2 }' | sed ':a;N;s/\n/,/g;ba' > CELAlreadyDown.csv"))
-      #Convierte esa csv en un vector
-      VecAlreadyDown<-as.vector(read.csv("CELAlreadyDown.csv"))
-    }
-  }
-
-
-#Si no se descargo completamente
-(names(GSMList(gse54002)))[!(names(GSMList(gse54002)) %in% vectc)]
-sapply(as.list((names(GSMList(gse54002)))[!(names(GSMList(gse54002)) %in% vectc)]),function(x){ getGEOSuppFiles(x)})
-
-
-
-
-DownCELFromThisGSMVec<-function(ThisGSMList){
-    for(i in 1:5){
-    sapply(ThisGSMList,function(x){ getGEOSuppFiles(x)})
-    #ponle un if para comprobar exito en la descarga si no que comience
-    try(system("ls GSM*/*CEL* |  wc -l > CELAlreadyDown.csv"))
-    CelAD<-as.numeric(read.table("CELAlreadyDown.csv"))
-      if( length(ThisGSMList) == CelAD ) {
-      return()
-      } 
-    }
-  }
-  
-  
-  try(system("ls *CEL* |  wc -l > CELAlreadyDown.csv"))
-  CelAD<-as.numeric(read.table("CELAlreadyDown.csv"))
+# Example of use
+MyGSE=c("GSE2327","GSE2298")
+GSMList_of_MyGSE<-GSMnamesFromVecGSEnames(MyGSE)
